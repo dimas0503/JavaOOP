0ec37143de03a7e8ca4274f078325f23bd69d2f4
dimas <dimas@com>
2015-10-10 19:15:00 +0300

    ExtArrayList 90 После вставки второго элемента 1 в начало, в списке нет элемента с индексом 0

--- src/homeWork1/MyArrayList.java
+++ src/homeWork1/MyArrayList.java
@@ -7,26 +7,36 @@ import java.util.Iterator;
  */
 
 public class MyArrayList <E> implements Iterable<E>{
-    private  int Capacity = 2;
+    private  int capacity = 2;
     private  int maxIndex = -1;
     private Object[] array;
 
     public MyArrayList() {
-        array = new Object[Capacity];
+        array = new Object[capacity];
     }
 
     public E get(int index){
-        return (E) array[index];
+        if (index <= maxIndex && index >=0){
+            return (E) array[index];
+        }
+        System.out.println("there's no such index " + index);
+        return null;
     }
 
 
     public boolean set(int index, E value){
+        //reject if out of bound
+        if (index > maxIndex || index < 0){
+            System.out.println("Error while replacing value. Your index " + index + " is out of bound of array");
+            return false;
+        }
+
         array[index] = value;
         return true;
     }
 
     public int indexOf(E value){
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i <=maxIndex; i++) {
             if (array[i] == value){
                 return i;
             }
@@ -35,49 +45,65 @@ public class MyArrayList <E> implements Iterable<E>{
     }
 
     public int size(){
-        int resSize = 0;
-        while ((resSize != array.length) && (array[resSize] != null)){
-            ++resSize;
-        }
-        return resSize;
+        return maxIndex+1;
     }
 
     public E remove(int index){
-        for (int i = index-1; array[i] != null ; i++) {
+        //reject removing out of bound
+        if (index > maxIndex || index < 0){
+            return null;
+        }
+
+        E res = (E) array[index];
+
+        //deleting element
+        array[index] = null;
+
+        //shifting other elements
+        for (int i = index; i < maxIndex ; i++) {
             array[i] = array[i+1];
         }
-        return (E) array;
+
+        --maxIndex;
+
+        return res;
     }
 
     public boolean add(int index, E value){
-        if (maxIndex != array.length-1){
-            int position = maxIndex+1;
-            while (position != index-1){
-                array[position] = array[position-1];
-                --position;
-            }
-            array[index-1] = value;
-            ++maxIndex;
-        }else {
-            Object[] tmpArray = new Object[array.length + Capacity];
+        //reject inserting  if out of bound
+        if (index > maxIndex || index < 0){
+            return false;
+        }
+
+        //extending array if not enough space
+        if (maxIndex == array.length-1){
+            Object[] tmpArray = new Object[array.length + capacity];
             System.arraycopy(array, 0, tmpArray, 0, array.length);
             array = tmpArray;
-            int position = maxIndex+1;
-            while (position != index-1){
-                array[position] = array[position-1];
-                --position;
-            }
-            array[index-1] = value;
-            ++maxIndex;
         }
+
+
+        //find place where to paste new element
+        int position = maxIndex;
+        while (position >= index){
+            array[position+1] = array[position];
+            --position;
+        }
+
+
+        //paste new value
+        array[index] = value;
+        ++maxIndex;
+
         return true;
     }
 
     public void add(E value){
+        //If current capacity enough to add just add. Else extending existing array and adding the value
         if (maxIndex != array.length-1){
             array[++maxIndex] = value;
         }else {
-            Object[] tmpArray = new Object[array.length+ Capacity];
+            Object[] tmpArray = new Object[array.length+ capacity];
             for (int i = 0; i < array.length; i++) {
                 tmpArray[i] = array[i];
             }
@@ -88,8 +114,9 @@ public class MyArrayList <E> implements Iterable<E>{
 
     public void print(){
         for (int i = 0; i < array.length && array[i] != null; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + " | ");
         }
+        System.out.println();
     }
 
     @Override
--- src/homeWork1/MyArrayListMain.java
+++ src/homeWork1/MyArrayListMain.java
@@ -5,34 +5,50 @@ package homeWork1;
  */
 public class MyArrayListMain{
     public static void main(String[] args) {
+/*        //for strings
         MyArrayList<String> arr = new MyArrayList<>();
-        MyArrayList<Integer> arr1 = new MyArrayList<>();
+        arr.add("Hello");
+        arr.add("Hello1");
+        arr.add("Hello2");
+        arr.print();
+        System.out.println("Get 0 index  -> " + arr.get(0));
+        System.out.println("Get 10 index  -> " + arr.get(10));
+        arr.set(10, "H");
+        arr.set(1, "H");
+        arr.print();
+        arr.add(2, "h1");
+        arr.print();
+        System.out.println("index of h --> " + arr.indexOf("Hello2"));
+        System.out.println("Size --> "+ arr.size());
+        System.out.println("remove " + arr.remove(3));
+        arr.print();
 
-        arr1.add(10);
-        arr1.add(20);
+        for (String element : arr){
+            System.out.print(element+" | ");
+        }
+*/
+
+        //for int
+        MyArrayList<Integer> arr1 = new MyArrayList<>();
+        arr1.add(1);
+        arr1.add(2);
+        arr1.add(0,7);
+        System.out.println(arr1.get(0));
+        arr1.add(1,8);
+        System.out.println(arr1.get(0));
+        arr1.print();
+
+/*
+        arr1.add(30);
+        arr1.print();
+        arr1.set(10, 2);
+        arr1.set(1,2);
+        arr1.print();
 
         for (int i: arr1){
             System.out.println(i);
         }
         arr1.remove(1);
-
-
-        arr.add("Hello");
-        arr.add("Hello1");
-        arr.add("Hello2");
-        arr.add("Hello3");
-        arr.add("Hello4");
-        arr.add("Hello5");
-        arr.add("Hello6");
-        arr.print();
-        arr.remove(7);
-        System.out.println("------------");
-        arr.print();
-
-        for (String element : arr){
-            System.out.println(element);
-        }
-
 
         arr.add(3,"Hello1.1");
         arr.print();
@@ -40,8 +56,7 @@ public class MyArrayListMain{
         System.out.println(arr.size());
 
         System.out.println(arr.get(3));
-
-
+*/
     }
 
 }
